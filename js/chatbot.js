b4a380c96bba1db5ccaf143ca5abc1e37fb127c7
munginisfree <s01099684205@gmail.com>
2024-04-11 15:49:50 +0900

    on my way...

--- js/chatbot.js
+++ js/chatbot.js
@@ -2,6 +2,8 @@ import taskData from "./taskData.js";
 
 console.log(taskData[2]);
 
+
+
 // 전역변수 정의
 // const $callyResponse = document.getElementById("callyResponse");
 const $input = document.getElementById("input");
@@ -21,11 +23,18 @@ let exceriseCount = 0;
 
 // 함수 정의
 
-// 입력받은 날짜를 기준으로 일주일간 가장 적게한 카테고리 리턴
+// 입력받은 날짜를 기준으로 주변 7개의 task간 가장 적게한 카테고리 리턴
 function countCategory(date){
     let min = work;
 
-    // 그 날자의 객체 날짜 전달
+    taskData[target];
+    for(i=1; i<3; i++){
+        taskData[target-i];
+        taskData[target+i];
+
+    }
+
+    // 그 날짜의 객체 날짜 전달
 
     return min;
 }
@@ -84,11 +93,12 @@ const returnCallyResponse = () => {
 // 유저로부터 날짜 데이터 받기
 function getDateFromUser() {
     const $date = document.createElement("div");
-    $date.textContent = "원하는 날짜를 입력하세요 (YYYY-MM-DD 형식으로 입력해주세요)";
+    $date.innerHTML = "수정을 원하는 날짜를 입력해주세요<br>(YYYY-MM-DD 형식으로 입력해주세요)";
     $response.appendChild($date);
     $date.classList.add('callyResponse');
+    
 
-    const $userDate = $response.lastElementChild.value;
+    const $userDate = $response.lastElementChild.textContent;
     const userDate = $userDate.trim();
     const userDateCopy = userDate;
 
@@ -100,10 +110,11 @@ function getDateFromUser() {
 
     if (userDate.length !== 10) {
         // 입력값이 제대로 되지 않았다면...
+        console.log("error");
         $inputError = document.createElement("div");
-        $inputError.textContent = "원하는 날짜를 입력하세요 (YYYY-MM-DD 형식으로 입력해주세요)";
         $response.appendChild($inputError);
         $inputError.classList.add('callyResponse');
+        $inputError.innerHTML = "YYYY-MM-DD 형식으로 입력해주세요";
     } else {
         // 사용자가 입력한 날짜데이터 년도, 월, 일로 추출
         const dateParts = userDateCopy.split("-");
@@ -121,14 +132,34 @@ function getDateFromUser() {
     return userDateInfo;
 }
 
+function getRecentTodo(date){
+    let index = date;
+    
+}
 function checkSchedule() {
     getDateFromUser();
+
+    getRecentTodo();
 
     // 입력받은 날짜를 기준으로 앞 뒤 3일 todoList.title 가져와서 출력
     
     // 출력하려면 todolist.title을 먼저 리스트에 담아서 그 갯수를 세고
-    // 갯수 만큼 li 추가해야함
+    // 갯수 만큼 div 추가해야함
+    // 각각의 todo를 담을 싸개
+    const $chekedSs = document.createElement('div');
+    $chekedSs.classList.add('callyResponse');
+    $response.appendChild($chekedSs);
 
+    // 추가하는 함수
+    // todoitems todoList에서 title객체만 뺴서 가져와서 todo 에 집어넣기
+
+    let todo = [];
+    for(let i = 0; i < todo.length; i++){
+        const $chekeditem = document.createElement('div');
+        $chekeditem.innerHTML = `${i+1}. ${todo[i]}`
+        $chekedSs.appendChild($chekeditem);
+
+    }
     // li 묶음을 #callyResponse에 추가
 }
 
@@ -148,14 +179,16 @@ function addSechdule() {
     $response.appendChild($newSchedule);
     // 입력받은 값 todo list에  또다른 객체로 추가
 }
-// 최근동안 어떠한 일을 많이 했는지 알아봄
-function checkRecentWork() {}
+// 최근동안 어떠한 일을 적게 했는지 알아봄
+function checkRecentWork(min) {
+    
+}
 // 챗봇에게 할일 추천 받기
 function recommendTodo() {
     // 어떤 날짜에 할 일을 추가하고 싶은지 물어봄
     getDateFromUser();
 
-    //입력 받은 날짜를 기반으로 최근동안 어떠한 일을 가장 많이 했는지 알려줌
+    //입력 받은 날짜를 기반으로 지금까지 있는 task 중 어떠한 일을 가장 많이 했는지 알려줌
     checkRecentWork();
 
     // 가장 많이한 일을 출력하며, 그 날짜를 기준으로 최근 일주일간 가장많이 한 일은 땡땡 이군요!
