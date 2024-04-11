01ffa3936c1b11d8f34c534d66414e57f7615747
munginisfree <s01099684205@gmail.com>
2024-04-11 22:34:30 +0900

    chatbot ongoing

--- js/chatbot.js
+++ js/chatbot.js
@@ -2,8 +2,6 @@ import taskData from "./taskData.js";
 
 console.log(taskData[2]);
 
-
-
 // 전역변수 정의
 // const $callyResponse = document.getElementById("callyResponse");
 const $input = document.getElementById("input");
@@ -16,22 +14,24 @@ const $chatbotContainer = document.getElementById("chatbot-container");
 const $chatPart = document.getElementById("chatPart");
 const $option = $chatPart.querySelector("li");
 
-// 카테고리 카운트
-let workCount = 0;
-let restCount = 0;
-let exceriseCount = 0;
-
 // 함수 정의
 
 // 입력받은 날짜를 기준으로 주변 7개의 task간 가장 적게한 카테고리 리턴
-function countCategory(date){
-    let min = work;
+function countCategory(date) {
+    let workCount = 0;
+    let restCount = 0;
+    let exceriseCount = 0;
+    let min = workCount;
+    
+    if(true){
 
+    }
     taskData[target];
-    for(i=1; i<3; i++){
-        taskData[target-i];
-        taskData[target+i];
+    for (i = 1; i < 3; i++) {
+        taskData[target - i];
 
+
+        taskData[target + i];
     }
 
     // 그 날짜의 객체 날짜 전달
@@ -40,9 +40,6 @@ function countCategory(date){
 }
 
 // 함수 실행
-
-
-
 
 //x누르면 화면 내리기
 $close.addEventListener("click", (e) => {
@@ -58,12 +55,6 @@ $input.addEventListener("input", (e) => {
     }
 });
 
-// 엔터누르면 전달됨
-$input.parentElement.addEventListener("submit", (e) => {
-    e.preventDefault();
-    returnNewTodo();
-});
-
 const returnNewTodo = () => {
     const newTodo = $input.querySelector("input").value;
     // 저장해둔 input 값을 채팅 li에 추가
@@ -74,31 +65,44 @@ const returnNewTodo = () => {
     // 입력 후 input 값 초기화
     // 왜 안돼묘??
     // newTodo = '';
-    $input.querySelector("input").value = '';
+    $input.querySelector("input").value = "";
 };
 
+// 엔터누르면 전달됨
+$input.parentElement.addEventListener("submit", (e) => {
+    e.preventDefault();
+    returnNewTodo();
+});
 // 보내기 버튼 누르면
 $arrow.addEventListener("click", (e) => {
     e.preventDefault();
     returnNewTodo();
 });
 
-const returnCallyResponse = () => {
-    // 첫번째 입력 받기
-    // 사용자가 직접 입력(1,2,3,4 나 일정조회, 일정추가, 일정추천, 직접입력/ 을 직접 입력하고 전송함)
-    // #chatPart 무엇을 도와드릴까요? 에서 4개중에 한개 선택함
+// const returnCallyResponse = () => {
+//     // 첫번째 입력 받기
+//     // 사용자가 직접 입력(1,2,3,4 나 일정조회, 일정추가, 일정추천, 직접입력/ 을 직접 입력하고 전송함)
+//     // #chatPart 무엇을 도와드릴까요? 에서 4개중에 한개 선택함
 
-};
+// };
 
+// const $userDate = (($input), e =>{
+//     $realInput = $input.querySelector('input');
+
+// })
 // 유저로부터 날짜 데이터 받기
 function getDateFromUser() {
     const $date = document.createElement("div");
-    $date.innerHTML = "수정을 원하는 날짜를 입력해주세요<br>(YYYY-MM-DD 형식으로 입력해주세요)";
+    $date.innerHTML =
+        "수정을 원하는 날짜를 입력해주세요<br>(YYYY-MM-DD 형식으로 입력해주세요)";
     $response.appendChild($date);
-    $date.classList.add('callyResponse');
-    
+    $date.classList.add("callyResponse");
+
+    // 사용자한테 날짜 입력받아야됨!!!!!
+    // 즉 li태그가 추가될 때까지 기다렸다가 그 값을 $userDate로 받아야함
 
     const $userDate = $response.lastElementChild.textContent;
+    console.log($userDate);
     const userDate = $userDate.trim();
     const userDateCopy = userDate;
 
@@ -111,10 +115,10 @@ function getDateFromUser() {
     if (userDate.length !== 10) {
         // 입력값이 제대로 되지 않았다면...
         console.log("error");
-        $inputError = document.createElement("div");
+        const $inputError = document.createElement("div");
+        $inputError.textContent = "어허~~ YYYY-MM-DD 형식으로 입력해주세요";
         $response.appendChild($inputError);
-        $inputError.classList.add('callyResponse');
-        $inputError.innerHTML = "YYYY-MM-DD 형식으로 입력해주세요";
+        $inputError.classList.add("callyResponse");
     } else {
         // 사용자가 입력한 날짜데이터 년도, 월, 일로 추출
         const dateParts = userDateCopy.split("-");
@@ -132,45 +136,42 @@ function getDateFromUser() {
     return userDateInfo;
 }
 
-function getRecentTodo(date){
+function getRecentTodo(date) {
     let index = date;
-       // 입력받은 날짜를 기준으로 앞 뒤 3일 todoList.title 가져와서 출력
-    
+    // 입력받은 날짜를 기준으로 앞 뒤 3일 todoList.title 가져와서 출력
+
     // 출력하려면 todolist.title을 먼저 리스트에 담아서 그 갯수를 세고
     // 갯수 만큼 div 추가해야함
     // 각각의 todo를 담을 싸개
-    const $chekedSs = document.createElement('div');
-    $chekedSs.classList.add('callyResponse');
+    const $chekedSs = document.createElement("div");
+    $chekedSs.classList.add("callyResponse");
     $response.appendChild($chekedSs);
 
     // 추가하는 함수
     // todoitems todoList에서 title객체만 뺴서 가져와서 todo 에 집어넣기
     let todo = [];
-    if(index > 3){
-        for(let i = 1; i <= 3; i++){
-                taskData[index - i];
+    if (index > 3) {
+        for (let i = 1; i <= 3; i++) {
+            taskData[index - i];
             taskData[index + i];
         }
-    }else{
+    } else {
         // ㅇㅖ외처리 해야함
-        for(let i = index; i <= (7-index); i++){
+        for (let i = index; i <= 7 - index; i++) {
             taskData[i];
         }
     }
-    for(let i = 0; i < todo.length; i++){
-        const $chekeditem = document.createElement('div');
-        $chekeditem.innerHTML = `${i+1}. ${todo[i]}`
+    for (let i = 0; i < todo.length; i++) {
+        const $chekeditem = document.createElement("div");
+        $chekeditem.innerHTML = `${i + 1}. ${todo[i]}`;
         $chekedSs.appendChild($chekeditem);
-
     }
-    
 }
 function checkSchedule() {
     getDateFromUser();
 
-    getRecentTodo();
+    // getRecentTodo();
 
- 
     // li 묶음을 #callyResponse에 추가
 }
 
@@ -191,9 +192,7 @@ function addSechdule() {
     // 입력받은 값 todo list에  또다른 객체로 추가
 }
 // 최근동안 어떠한 일을 적게 했는지 알아봄
-function checkRecentWork(min) {
-    
-}
+function checkRecentWork(min) {}
 // 챗봇에게 할일 추천 받기
 function recommendTodo() {
     // 어떤 날짜에 할 일을 추가하고 싶은지 물어봄
@@ -211,11 +210,9 @@ function customByUser() {
     // 아직 지원하지 않는 기능입니다.
     const $notYet = document.createElement("div");
     $notYet.textContent = "현재 지원하지 않는 기능입니다.";
-    $notYet.classList.add('callyResponse');
+    $notYet.classList.add("callyResponse");
     $response.appendChild($notYet);
 }
-
-
 
 $chatPart.addEventListener("click", (e) => {
     // Check if the clicked element is a <button> tag
